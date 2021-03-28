35678cb871b6ad83ddd80d969badfaafa157ae30
Woldemar <Woldemar8888@gmail.com>
2021-03-28 15:45:33 +0300

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from "./Todo/TodoList";
+import {useState} from  'react';
 
 function App() {
+    let [todos, setTodus] = useState( [
+        {id:1, completed: true, title: 'buy some bread'},
+        {id:2, completed: false, title: 'walk'},
+        {id:3, completed: false, title: 'sleep'},
+    ]);
+
+    function toggleTodo(id){
+        setTodus(todos.map(todo => {
+            if(todo.id === id){
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        }))
+    }
+
+    function deleteTodo(id){
+        setTodus(todos.filter(todo => todo.id !== id ))
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Todo list</h1>
+        <TodoList
+          todos = {todos}
+          onToggle = {toggleTodo}
+          delete = {deleteTodo}
+        />
     </div>
   );
 }
